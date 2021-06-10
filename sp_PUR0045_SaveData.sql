98b81b5bbadcd3e344747b8ea5f65377996bec25
peoruby <peoruby@gmail.com>
2021-06-10 15:23:34 +0700

    ver 0

--- sp_PUR0045_SaveData.sql
+++ sp_PUR0045_SaveData.sql
@@ -48,107 +48,107 @@ set @PType = (select [ITEM_TYPE] from OPENXML(@iXML,'/DocumentElement/XmlUpdate'
 if @PType = 'Material' 
  begin      
   if (select top 1 1 from PARAMERF where item_code=@hinb)=1    
-	BEGIN
+  BEGIN
 
-		  ----------------------------------------------------------= Plastc =----------------------------------------------------------------------------      
-		  Update P      
-		  set P.PLA_YOBI6 = xm.[LEAD_TIME],P.ANZEN_ZAIKO_SU = xm.[SAFETY_STOCK],P.PLA_YOBI1 = xm.[ROUND_QUANTITY],P.UNIT = xm.[UNIT],P.MOQ = xm.[MOQ]      
-		   --,P.UNIT_PRICE = xm.[UNIT_PRICE],
-		  --, P.CURRENCY = xm.[CURRENCY],
+  ----------------------------------------------------------= Plastc =----------------------------------------------------------------------------      
+  Update P      
+        
+  set P.PLA_YOBI6 = xm.[LEAD_TIME],P.ANZEN_ZAIKO_SU = xm.[SAFETY_STOCK],P.PLA_YOBI1 = xm.[ROUND_QUANTITY],P.UNIT = xm.[UNIT],P.MOQ = xm.[MOQ]      
+   --,P.UNIT_PRICE = xm.[UNIT_PRICE],
+  --, P.CURRENCY = xm.[CURRENCY],
 
-		 , P.TRADE_TERM = xm.[TRADE_TERM],P.FORECAST_REQ = xm.[FORECAST_REQ]      
-		   ,P.TRANSPORTATION = xm.[TRANSPORTATION],P.CUSTOMER_CLEARANCE = xm.[CUSTOMER_CLEARANCE]      
-		   ,P.DATA_KUBUN = xm.[MSC],P.CUSTOMER_FLAG = xm.[CUSTOMER_FLAG],P.PERCENT_NG_RATE = xm.[PERCENT_NG_RATE],P.EXP_CONTROL = xm.[EXP_CONTROL]      
-		   ,P.EXP_DATE = xm.[EXP_DATE],P.FIFO_LOT = xm.[FIFO_LOT],P.FIFO_INPUT_DATE = xm.[FIFO_INPUT_DATE]      
-		  , P.Safety_LT = XM.[Safety_LT],P.Prod_LT = XM.[Prod_LT]
-		  ,P.HACHU_PLAN_JOUGEN = xm.[UPPER_ORDER_PLAN],P.PLA_YOBI2 = XM.[EXTEND_POCP]
-		   ,p.QuoNo = xm.QuoNo,p.QuoDate = xm.QuoDate,p.EffectiveDate = xm.EffDate,p.Comment = xm.Comment
-		   ,p.Shortage_Rate = xm.ShRate
+ , P.TRADE_TERM = xm.[TRADE_TERM],P.FORECAST_REQ = xm.[FORECAST_REQ]      
+   ,P.TRANSPORTATION = xm.[TRANSPORTATION],P.CUSTOMER_CLEARANCE = xm.[CUSTOMER_CLEARANCE]      
+   ,P.DATA_KUBUN = xm.[MSC],P.CUSTOMER_FLAG = xm.[CUSTOMER_FLAG],P.PERCENT_NG_RATE = xm.[PERCENT_NG_RATE],P.EXP_CONTROL = xm.[EXP_CONTROL]      
+   ,P.EXP_DATE = xm.[EXP_DATE],P.FIFO_LOT = xm.[FIFO_LOT],P.FIFO_INPUT_DATE = xm.[FIFO_INPUT_DATE]      
+  , P.Safety_LT = XM.[Safety_LT],P.Prod_LT = XM.[Prod_LT]
+  ,P.HACHU_PLAN_JOUGEN = xm.[UPPER_ORDER_PLAN],P.PLA_YOBI2 = XM.[EXTEND_POCP]
+   ,p.QuoNo = xm.QuoNo,p.QuoDate = xm.QuoDate,p.EffectiveDate = xm.EffDate,p.Comment = xm.Comment
+   ,p.Shortage_Rate = xm.ShRate
 
-		  from  PLASTCMF P      
-		  inner join OPENXML(@iXML,'/DocumentElement/XmlUpdate',2) WITH( ITEM_CODE varchar(25),LEAD_TIME smallint,SAFETY_STOCK numeric(18,2),MSC char(1),      
-			 ROUND_QUANTITY numeric(18,2),UNIT varchar(40),MOQ numeric(18,2),UNIT_PRICE numeric(18,4),CURRENCY varchar(5),TRADE_TERM varchar(30),      
-			 FORECAST_REQ int,TRANSPORTATION varchar(30),CUSTOMER_CLEARANCE int,FIFO_LOT char(1),CUSTOMER_FLAG char(1),      
-			 PERCENT_NG_RATE char(1),EXP_CONTROL char(1),EXP_DATE numeric(9,2),FIFO_INPUT_DATE char(1), Safety_LT int, Prod_LT int, EXTEND_POCP int,UPPER_ORDER_PLAN int
-			  ,QuoNo varchar(100),QuoDate datetime,EffDate datetime,Comment nvarchar(500),ShRate float)xm on P.ZAIRYO_HINBAN = xm.ITEM_CODE           
-		  ------------------------------------------------------------------------------------------------------------------------------------------------      
-			   Update PR    
-		   SET ITEM_TYPE=@PType,SUPPLIER_CODE=xm.SUPPLIER_CODE,LEAD_TIME=xm.LEAD_TIME,SAFETY_STOCK=xm.SAFETY_STOCK,ROUND_QUANTITY=xm.ROUND_QUANTITY,UNIT=xm.UNIT,MOQ=xm.MOQ,    
-				 TRADE_TERM=xm.TRADE_TERM,FORECAST_REQ=xm.FORECAST_REQ,TRANSPORTATION=xm.TRANSPORTATION,CUSTOMER_CLEARANCE=xm.CUSTOMER_CLEARANCE,
-				 UPPER_ORDER_PLAN=xm.UPPER_ORDER_PLAN ,
-				 MSC=xm.MSC      
-			   --,CHEMICAL_FLAG=xm.CHEMICAL_FLAG,
-			   ,CUSTOMER_FLAG=xm.CUSTOMER_FLAG,PERCENT_NG_RATE=xm.PERCENT_NG_RATE,EXP_CONTROL=xm.EXP_CONTROL,EXP_DATE=xm.EXP_DATE,FIFO_LOT=xm.FIFO_LOT,FIFO_INPUT_DATE=xm.FIFO_INPUT_DATE   
-			,INPUT_PERSON=@M_SAKUSEI,INPUT_DATE   =GETDATE() 
-			 ,Pr.Safety_LT = XM.[Safety_LT],Pr.Prod_LT = XM.[Prod_LT]
-			   --,Safety_LT=xm.Safety_LT,Prod_LT=xm.Prod_LT,
-			  , EXTEND_POCP =xm.EXTEND_POCP  
-			  ,pr.QuoNo = xm.QuoNo,pr.QuoDate = xm.QuoDate,pr.EffectiveDate = xm.EffDate,pr.Comment = xm.Comment
-			  ,pr.Shortage_Rate = xm.ShRate
-		  ----set P.HA_READ_TIME = xm.[LEAD_TIME],P.ANZEN_ZAIKO = xm.[SAFETY_STOCK],P.MARUME_SURYO = xm.[ROUND_QUANTITY],P.UNIT = xm.[UNIT],P.MOQ = xm.[MOQ]      
-		  ---- --,P.UNIT_PRICE = xm.[UNIT_PRICE],P.CURRENCY = xm.[CURRENCY]    
-		  ---- ,P.TRADE_TERM = xm.[TRADE_TERM],P.FORECAST_REQ = xm.[FORECAST_REQ]      
-		  ---- ,P.TRANSPORTATION = xm.[TRANSPORTATION],P.CAN_DRUM = xm.[CAN_DRUM],P.CUSTOMER_CLEARANCE = xm.[CUSTOMER_CLEARANCE]      
-		  ---- ,P.HACHU_PLAN_JOUGEN = xm.[UPPER_ORDER_PLAN],P.PA_FLAG1 = xm.[MSC],P.C_FLAG = xm.[CHEMICAL_FLAG]      
-		  ---- ,P.CUSTOMER_FLAG = xm.[CUSTOMER_FLAG],P.PERCENT_NG_RATE = xm.[PERCENT_NG_RATE],P.EXP_CONTROL = xm.[EXP_CONTROL],P.MASTER_UP_OP = @M_SAKUSEI      
-		  ---- ,P.EXP_DATE = xm.[EXP_DATE],P.FIFO_LOT = xm.[FIFO_LOT],P.FIFO_INPUT_DATE = xm.[FIFO_INPUT_DATE],P.MASTER_UP_DATE = GETDATE()      
-		  ---- ,,P.PLA_YOBI2 = XM.[EXTEND_POCP], P.Packing_Flag = XM.[Packing_Flag]      
+  from  PLASTCMF P      
+  inner join OPENXML(@iXML,'/DocumentElement/XmlUpdate',2) WITH( ITEM_CODE varchar(25),LEAD_TIME smallint,SAFETY_STOCK numeric(18,2),MSC char(1),      
+     ROUND_QUANTITY numeric(18,2),UNIT varchar(40),MOQ numeric(18,2),UNIT_PRICE numeric(18,4),CURRENCY varchar(5),TRADE_TERM varchar(30),      
+     FORECAST_REQ int,TRANSPORTATION varchar(30),CUSTOMER_CLEARANCE int,FIFO_LOT char(1),CUSTOMER_FLAG char(1),      
+     PERCENT_NG_RATE char(1),EXP_CONTROL char(1),EXP_DATE numeric(9,2),FIFO_INPUT_DATE char(1), Safety_LT int, Prod_LT int, EXTEND_POCP int,UPPER_ORDER_PLAN int
+	  ,QuoNo varchar(100),QuoDate datetime,EffDate datetime,Comment nvarchar(500),ShRate float)xm on P.ZAIRYO_HINBAN = xm.ITEM_CODE           
+  ------------------------------------------------------------------------------------------------------------------------------------------------      
+           Update PR    
+   SET ITEM_TYPE=@PType,SUPPLIER_CODE=xm.SUPPLIER_CODE,LEAD_TIME=xm.LEAD_TIME,SAFETY_STOCK=xm.SAFETY_STOCK,ROUND_QUANTITY=xm.ROUND_QUANTITY,UNIT=xm.UNIT,MOQ=xm.MOQ,    
+         TRADE_TERM=xm.TRADE_TERM,FORECAST_REQ=xm.FORECAST_REQ,TRANSPORTATION=xm.TRANSPORTATION,CUSTOMER_CLEARANCE=xm.CUSTOMER_CLEARANCE,
+		 UPPER_ORDER_PLAN=xm.UPPER_ORDER_PLAN ,
+		 MSC=xm.MSC      
+       --,CHEMICAL_FLAG=xm.CHEMICAL_FLAG,
+	   ,CUSTOMER_FLAG=xm.CUSTOMER_FLAG,PERCENT_NG_RATE=xm.PERCENT_NG_RATE,EXP_CONTROL=xm.EXP_CONTROL,EXP_DATE=xm.EXP_DATE,FIFO_LOT=xm.FIFO_LOT,FIFO_INPUT_DATE=xm.FIFO_INPUT_DATE   
+    ,INPUT_PERSON=@M_SAKUSEI,INPUT_DATE   =GETDATE() 
+	 ,Pr.Safety_LT = XM.[Safety_LT],Pr.Prod_LT = XM.[Prod_LT]
+       --,Safety_LT=xm.Safety_LT,Prod_LT=xm.Prod_LT,
+	  , EXTEND_POCP =xm.EXTEND_POCP  
+      ,pr.QuoNo = xm.QuoNo,pr.QuoDate = xm.QuoDate,pr.EffectiveDate = xm.EffDate,pr.Comment = xm.Comment
+	  ,pr.Shortage_Rate = xm.ShRate
+  ----set P.HA_READ_TIME = xm.[LEAD_TIME],P.ANZEN_ZAIKO = xm.[SAFETY_STOCK],P.MARUME_SURYO = xm.[ROUND_QUANTITY],P.UNIT = xm.[UNIT],P.MOQ = xm.[MOQ]      
+  ---- --,P.UNIT_PRICE = xm.[UNIT_PRICE],P.CURRENCY = xm.[CURRENCY]    
+  ---- ,P.TRADE_TERM = xm.[TRADE_TERM],P.FORECAST_REQ = xm.[FORECAST_REQ]      
+  ---- ,P.TRANSPORTATION = xm.[TRANSPORTATION],P.CAN_DRUM = xm.[CAN_DRUM],P.CUSTOMER_CLEARANCE = xm.[CUSTOMER_CLEARANCE]      
+  ---- ,P.HACHU_PLAN_JOUGEN = xm.[UPPER_ORDER_PLAN],P.PA_FLAG1 = xm.[MSC],P.C_FLAG = xm.[CHEMICAL_FLAG]      
+  ---- ,P.CUSTOMER_FLAG = xm.[CUSTOMER_FLAG],P.PERCENT_NG_RATE = xm.[PERCENT_NG_RATE],P.EXP_CONTROL = xm.[EXP_CONTROL],P.MASTER_UP_OP = @M_SAKUSEI      
+  ---- ,P.EXP_DATE = xm.[EXP_DATE],P.FIFO_LOT = xm.[FIFO_LOT],P.FIFO_INPUT_DATE = xm.[FIFO_INPUT_DATE],P.MASTER_UP_DATE = GETDATE()      
+  ---- ,,P.PLA_YOBI2 = XM.[EXTEND_POCP], P.Packing_Flag = XM.[Packing_Flag]      
            
-		  from  PARAMERF PR      
-		  inner join OPENXML(@iXML,'/DocumentElement/XmlUpdate',2) WITH( ITEM_CODE varchar(25),SUPPLIER_CODE int,LEAD_TIME smallint,SAFETY_STOCK numeric(18,2),      
-			 ROUND_QUANTITY numeric(18,2),UNIT varchar(40),MOQ numeric(18,2),UNIT_PRICE numeric(18,4),CURRENCY varchar(5),TRADE_TERM varchar(30),      
-			 FORECAST_REQ int,TRANSPORTATION varchar(30),CUSTOMER_CLEARANCE int,MSC char(1),CUSTOMER_FLAG char(1),PERCENT_NG_RATE char(1),      
-			 EXP_CONTROL char(1),EXP_DATE numeric(9,2),FIFO_LOT char(1),FIFO_INPUT_DATE char(1),UNIT_PRICE_USD numeric(18,4), Safety_LT int, Prod_LT int, EXTEND_POCP int,UPPER_ORDER_PLAN int
-			 ,QuoNo varchar(100),QuoDate datetime,EffDate datetime,Comment nvarchar(500),ShRate float)xm  ON XM.ITEM_CODE=PR.ITEM_CODE 
-	END  
+  from  PARAMERF PR      
+  inner join OPENXML(@iXML,'/DocumentElement/XmlUpdate',2) WITH( ITEM_CODE varchar(25),SUPPLIER_CODE int,LEAD_TIME smallint,SAFETY_STOCK numeric(18,2),      
+     ROUND_QUANTITY numeric(18,2),UNIT varchar(40),MOQ numeric(18,2),UNIT_PRICE numeric(18,4),CURRENCY varchar(5),TRADE_TERM varchar(30),      
+     FORECAST_REQ int,TRANSPORTATION varchar(30),CUSTOMER_CLEARANCE int,MSC char(1),CUSTOMER_FLAG char(1),PERCENT_NG_RATE char(1),      
+     EXP_CONTROL char(1),EXP_DATE numeric(9,2),FIFO_LOT char(1),FIFO_INPUT_DATE char(1),UNIT_PRICE_USD numeric(18,4), Safety_LT int, Prod_LT int, EXTEND_POCP int,UPPER_ORDER_PLAN int
+	 ,QuoNo varchar(100),QuoDate datetime,EffDate datetime,Comment nvarchar(500),ShRate float)xm  ON XM.ITEM_CODE=PR.ITEM_CODE 
+end  
 ELSE
-	Begin
-	  ----------------------------------------------------------= Paramerf =--------------------------------------------------------------------------      
-	  INSERT INTO PARAMERF(DATA_NO,YEN_USD,VND_USD,ITEM_TYPE,ITEM_CODE,SUPPLIER_CODE,LEAD_TIME,SAFETY_STOCK,ROUND_QUANTITY,UNIT,MOQ      
-		   ,TRADE_TERM,FORECAST_REQ,TRANSPORTATION,CUSTOMER_CLEARANCE,MSC,CUSTOMER_FLAG,PERCENT_NG_RATE      
-		   ,EXP_CONTROL,EXP_DATE,FIFO_LOT,FIFO_INPUT_DATE,INPUT_PERSON,INPUT_DATE,Safety_LT,Prod_LT,EXTEND_POCP,UPPER_ORDER_PLAN,QuoNo,QuoDate,EffectiveDate,Comment,Shortage_Rate)      
+
+  ----------------------------------------------------------= Paramerf =--------------------------------------------------------------------------      
+  INSERT INTO PARAMERF(DATA_NO,YEN_USD,VND_USD,ITEM_TYPE,ITEM_CODE,SUPPLIER_CODE,LEAD_TIME,SAFETY_STOCK,ROUND_QUANTITY,UNIT,MOQ      
+       ,TRADE_TERM,FORECAST_REQ,TRANSPORTATION,CUSTOMER_CLEARANCE,MSC,CUSTOMER_FLAG,PERCENT_NG_RATE      
+       ,EXP_CONTROL,EXP_DATE,FIFO_LOT,FIFO_INPUT_DATE,INPUT_PERSON,INPUT_DATE,Safety_LT,Prod_LT,EXTEND_POCP,UPPER_ORDER_PLAN,QuoNo,QuoDate,EffectiveDate,Comment,Shortage_Rate)      
              
-	  select @Juchuno,@Yen,@Vnd,@PType,xm.ITEM_CODE,xm.SUPPLIER_CODE,xm.LEAD_TIME,xm.SAFETY_STOCK,xm.ROUND_QUANTITY,xm.UNIT,xm.MOQ      
-		,xm.TRADE_TERM,xm.FORECAST_REQ,xm.TRANSPORTATION,xm.CUSTOMER_CLEARANCE,xm.MSC,xm.CUSTOMER_FLAG      
-		,xm.PERCENT_NG_RATE,xm.EXP_CONTROL,xm.EXP_DATE,xm.FIFO_LOT,xm.FIFO_INPUT_DATE,@M_SAKUSEI,GETDATE()  ,XM.[Safety_LT]   ,XM.[Prod_LT],xm.EXTEND_POCP ,xm.UPPER_ORDER_PLAN    
-		,xm.QuoNo,xm.QuoDate,xm.EffDate,xm.Comment,xm.ShRate
+  select @Juchuno,@Yen,@Vnd,@PType,xm.ITEM_CODE,xm.SUPPLIER_CODE,xm.LEAD_TIME,xm.SAFETY_STOCK,xm.ROUND_QUANTITY,xm.UNIT,xm.MOQ      
+    ,xm.TRADE_TERM,xm.FORECAST_REQ,xm.TRANSPORTATION,xm.CUSTOMER_CLEARANCE,xm.MSC,xm.CUSTOMER_FLAG      
+    ,xm.PERCENT_NG_RATE,xm.EXP_CONTROL,xm.EXP_DATE,xm.FIFO_LOT,xm.FIFO_INPUT_DATE,@M_SAKUSEI,GETDATE()  ,XM.[Safety_LT]   ,XM.[Prod_LT],xm.EXTEND_POCP ,xm.UPPER_ORDER_PLAN    
+	,xm.QuoNo,xm.QuoDate,xm.EffDate,xm.Comment,xm.ShRate
         
-	  from OPENXML(@iXML,'/DocumentElement/XmlUpdate',2) WITH( ITEM_CODE varchar(25),SUPPLIER_CODE int,LEAD_TIME smallint,SAFETY_STOCK numeric(18,2),      
-		 ROUND_QUANTITY numeric(18,2),UNIT varchar(40),MOQ numeric(18,2),UNIT_PRICE numeric(18,4),CURRENCY varchar(5),TRADE_TERM varchar(30),      
-		 FORECAST_REQ int,TRANSPORTATION varchar(30),CUSTOMER_CLEARANCE int,MSC char(1),CUSTOMER_FLAG char(1),PERCENT_NG_RATE char(1),      
-		 EXP_CONTROL char(1),EXP_DATE numeric(9,2),FIFO_LOT char(1),FIFO_INPUT_DATE char(1),UNIT_PRICE_USD numeric(18,4), Safety_LT int, Prod_LT int,EXTEND_POCP int,UPPER_ORDER_PLAN int
-		 ,QuoNo varchar(100),QuoDate datetime,EffDate datetime,Comment nvarchar(500),ShRate float)xm           
-	  ------------------------------------------------------------------------------------------------------------------------------------------------         
+  from OPENXML(@iXML,'/DocumentElement/XmlUpdate',2) WITH( ITEM_CODE varchar(25),SUPPLIER_CODE int,LEAD_TIME smallint,SAFETY_STOCK numeric(18,2),      
+     ROUND_QUANTITY numeric(18,2),UNIT varchar(40),MOQ numeric(18,2),UNIT_PRICE numeric(18,4),CURRENCY varchar(5),TRADE_TERM varchar(30),      
+     FORECAST_REQ int,TRANSPORTATION varchar(30),CUSTOMER_CLEARANCE int,MSC char(1),CUSTOMER_FLAG char(1),PERCENT_NG_RATE char(1),      
+     EXP_CONTROL char(1),EXP_DATE numeric(9,2),FIFO_LOT char(1),FIFO_INPUT_DATE char(1),UNIT_PRICE_USD numeric(18,4), Safety_LT int, Prod_LT int,EXTEND_POCP int,UPPER_ORDER_PLAN int
+	 ,QuoNo varchar(100),QuoDate datetime,EffDate datetime,Comment nvarchar(500),ShRate float)xm           
+  ------------------------------------------------------------------------------------------------------------------------------------------------         
  
-	   Update P      
+   Update P      
         
-	  set P.PLA_YOBI6 = xm.[LEAD_TIME],P.ANZEN_ZAIKO_SU = xm.[SAFETY_STOCK],P.PLA_YOBI1 = xm.[ROUND_QUANTITY],P.UNIT = xm.[UNIT],P.MOQ = xm.[MOQ]      
-	   --,P.UNIT_PRICE = xm.[UNIT_PRICE],
-	  --, P.CURRENCY = xm.[CURRENCY],
+  set P.PLA_YOBI6 = xm.[LEAD_TIME],P.ANZEN_ZAIKO_SU = xm.[SAFETY_STOCK],P.PLA_YOBI1 = xm.[ROUND_QUANTITY],P.UNIT = xm.[UNIT],P.MOQ = xm.[MOQ]      
+   --,P.UNIT_PRICE = xm.[UNIT_PRICE],
+  --, P.CURRENCY = xm.[CURRENCY],
 
-	 , P.TRADE_TERM = xm.[TRADE_TERM],P.FORECAST_REQ = xm.[FORECAST_REQ]      
-	   ,P.TRANSPORTATION = xm.[TRANSPORTATION],P.CUSTOMER_CLEARANCE = xm.[CUSTOMER_CLEARANCE]      
-	   ,P.DATA_KUBUN = xm.[MSC],P.CUSTOMER_FLAG = xm.[CUSTOMER_FLAG],P.PERCENT_NG_RATE = xm.[PERCENT_NG_RATE],P.EXP_CONTROL = xm.[EXP_CONTROL]      
-	   ,P.EXP_DATE = xm.[EXP_DATE],P.FIFO_LOT = xm.[FIFO_LOT],P.FIFO_INPUT_DATE = xm.[FIFO_INPUT_DATE]  ,P.Safety_LT = XM.[Safety_LT],P.Prod_LT = XM.[Prod_LT]    
-	   , P.HACHU_PLAN_JOUGEN = xm.[UPPER_ORDER_PLAN],P.PLA_YOBI2 = XM.[EXTEND_POCP]
-	   ,p.QuoNo = xm.QuoNo,p.QuoDate = xm.QuoDate,p.EffectiveDate = xm.EffDate,p.Comment = xm.Comment
-	   ,p.Shortage_Rate = xm.ShRate
-	  from  PLASTCMF P      
-	  inner join OPENXML(@iXML,'/DocumentElement/XmlUpdate',2) WITH( ITEM_CODE varchar(25),LEAD_TIME smallint,SAFETY_STOCK numeric(18,2),MSC char(1),      
-		 ROUND_QUANTITY numeric(18,2),UNIT varchar(40),MOQ numeric(18,2),UNIT_PRICE numeric(18,4),CURRENCY varchar(5),TRADE_TERM varchar(30),      
-		 FORECAST_REQ int,TRANSPORTATION varchar(30),CUSTOMER_CLEARANCE int,FIFO_LOT char(1),CUSTOMER_FLAG char(1),      
-		 PERCENT_NG_RATE char(1),EXP_CONTROL char(1),EXP_DATE numeric(9,2),FIFO_INPUT_DATE char(1),Safety_LT int, Prod_LT int,EXTEND_POCP int,UPPER_ORDER_PLAN int
-		 ,QuoNo varchar(100),QuoDate datetime,EffDate datetime,Comment nvarchar(500),ShRate float)xm on P.ZAIRYO_HINBAN = xm.ITEM_CODE           
+ , P.TRADE_TERM = xm.[TRADE_TERM],P.FORECAST_REQ = xm.[FORECAST_REQ]      
+   ,P.TRANSPORTATION = xm.[TRANSPORTATION],P.CUSTOMER_CLEARANCE = xm.[CUSTOMER_CLEARANCE]      
+   ,P.DATA_KUBUN = xm.[MSC],P.CUSTOMER_FLAG = xm.[CUSTOMER_FLAG],P.PERCENT_NG_RATE = xm.[PERCENT_NG_RATE],P.EXP_CONTROL = xm.[EXP_CONTROL]      
+   ,P.EXP_DATE = xm.[EXP_DATE],P.FIFO_LOT = xm.[FIFO_LOT],P.FIFO_INPUT_DATE = xm.[FIFO_INPUT_DATE]  ,P.Safety_LT = XM.[Safety_LT],P.Prod_LT = XM.[Prod_LT]    
+   , P.HACHU_PLAN_JOUGEN = xm.[UPPER_ORDER_PLAN],P.PLA_YOBI2 = XM.[EXTEND_POCP]
+   ,p.QuoNo = xm.QuoNo,p.QuoDate = xm.QuoDate,p.EffectiveDate = xm.EffDate,p.Comment = xm.Comment
+   ,p.Shortage_Rate = xm.ShRate
+  from  PLASTCMF P      
+  inner join OPENXML(@iXML,'/DocumentElement/XmlUpdate',2) WITH( ITEM_CODE varchar(25),LEAD_TIME smallint,SAFETY_STOCK numeric(18,2),MSC char(1),      
+     ROUND_QUANTITY numeric(18,2),UNIT varchar(40),MOQ numeric(18,2),UNIT_PRICE numeric(18,4),CURRENCY varchar(5),TRADE_TERM varchar(30),      
+     FORECAST_REQ int,TRANSPORTATION varchar(30),CUSTOMER_CLEARANCE int,FIFO_LOT char(1),CUSTOMER_FLAG char(1),      
+     PERCENT_NG_RATE char(1),EXP_CONTROL char(1),EXP_DATE numeric(9,2),FIFO_INPUT_DATE char(1),Safety_LT int, Prod_LT int,EXTEND_POCP int,UPPER_ORDER_PLAN int
+	 ,QuoNo varchar(100),QuoDate datetime,EffDate datetime,Comment nvarchar(500),ShRate float)xm on P.ZAIRYO_HINBAN = xm.ITEM_CODE           
 
-	 end   
-	 End
+ end      
 ---------------------------------------------------------------------------------------------------------------------------------------------------------      
        
  set @strError = 'OK'      
       
  /* ======== Insert LogFile ======== */      
  INSERT INTO LOGFILEWK ( HostName, LOGVALUE)      
- select @Computer, convert(varchar(10),getdate(),3) + '  ' + convert(varchar(10),getdate(),108) + ',PUR0045,' + @Operator + ',' +      
+ select @Computer, convert(varchar(10),getdate(),3) + '  ' + convert(varchar(10),getdate(),108) + ',PUR0040,' + @Operator + ',' +      
  xm.ITEM_CODE + ',' + isnull(convert(varchar(20),xm.LEAD_TIME),'') + ',' + isnull(CONVERT(varchar(20),xm.SAFETY_STOCK), '') + ',' +       
  isnull(convert(varchar(22),xm.ROUND_QUANTITY),'0') + ',' + ISNULL(convert(varchar(20),xm.UPPER_ORDER_PLAN),'') + ',' +       
  isnull(convert(varchar(20),xm.EXTEND_POCP),'') + ',' + isnull(xm.MSC,'') + ',' + isnull(xm.Packing_Flag,'')  + ',' +        
@@ -164,7 +164,7 @@ ELSE
       
 exec sp_xml_removedocument @iXML      
       
-
+      
       
  COMMIT TRAN      
 END TRY      
@@ -179,3 +179,4 @@ END TRY
        
       
 END 
+GO
